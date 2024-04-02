508c6e9291c5f4bdddca0d3012bdfb2fa34905d4
Noxthe <83797031+Noxthe@users.noreply.github.com>
2024-04-01 23:52:56 -0600

    Add files via upload

---
+++ programa.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int cookie;
+char buf[10];
+printf("buf: %08x cookie: %08x\n", &buf, &cookie);
+gets(buf);
+if (cookie == 0x000d0a00)
+printf("¡ganaste myriam!\n");
+}
+
